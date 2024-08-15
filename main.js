62b37536cfbf10daead8d6cb09a4edc1a96efe4b
Skapershik <adsroslyakovsergey@yandex.ru>
2024-08-15 11:48:20 +0400

    change overlay z-index

--- main.js
+++ main.js
@@ -30,7 +30,7 @@
             overlay.style.position = "absolute";
             overlay.style.top = overlay.style.left = "0";
             overlay.style.width = overlay.style.height = "100%";
-            overlay.style.zIndex = "10";
+            overlay.style.zIndex = "0";
             overlay.style.opacity = opacity; // Apply the opacity here
             // Overlay is appended as a child of the original image's parent element (the thing we did query select), making it go on top
             thumbnail.parentElement.appendChild(overlay);
